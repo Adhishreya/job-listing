4aa9fa736f029eca3cce447b4a3086d53c2d1259
Android Nougat <42477290+Adhishreya@users.noreply.github.com>
2022-05-25 11:39:29 +0530

    form array

--- src/app/demo-components/second/second.component.ts
+++ src/app/demo-components/second/second.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-second',
@@ -6,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./second.component.scss']
 })
 export class SecondComponent implements OnInit {
+  dynamicForm = this.fb.group(
+    {
+      firstName: ['', Validators.required],
+      lastName: [''],
+      address: this.fb.group({
+        street: [''],
+        city: [''],
+        state: [''],
+        zip: ['']
+      }),
+      aliases: this.fb.array([
+        this.fb.control('')
+      ])
+    }
+  );
 
-  constructor() { }
+  get aliases() {
+    return this.dynamicForm.get('aliases') as FormArray;
+  }
+  constructor(private fb: FormBuilder) { }
 
+  addAlias() {
+    this.aliases.push(this.fb.control(''))
+  }
   ngOnInit(): void {
   }
 
