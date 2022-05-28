c0afc40232a8d3d331b0c7910d7608f26b4ba3df
Android Nougat <42477290+Adhishreya@users.noreply.github.com>
2022-05-28 21:37:05 +0530

    brute force fiter design

--- src/app/components/job-lists/job-lists.component.ts
+++ src/app/components/job-lists/job-lists.component.ts
@@ -15,19 +15,69 @@ export class JobListsComponent implements OnInit {
   constructor(private jobLIst: JobFetchService) { }
 
   ngOnInit(): void {
-    this.jobLIst.getJobs().subscribe((job) => this.jobs = job);
-  }
-  checkFilter(value: string) {
-    if (!this.filters.includes(value))
-      this.filters.push(value);
-    console.log(this.filters);
+    // this.jobLIst.getJobs().subscribe((job) => this.jobs = job);
+    this.fetchData();
   }
 
+
+  applyFilter(operation: string, value?: string) {
+    if (operation === "add") {
+      this.jobs = this.jobs.filter((job) => {
+        if (job.tools?.includes(value!) || job.role?.includes(value!) || job.level?.includes(value!) || job.languages?.includes(value!))
+          return true;
+        return false;
+      });
+    }
+    else {
+      // this.jobs = this.jobs.filter((job) => {
+      //   let present = false;
+
+      //   this.filters.every((element: string) => {
+      //     if (job.tools?.includes(element) || job.role?.includes(element) || job.level?.includes(element) || job.languages?.includes(element)) {
+      //       present = true;
+      //     }
+      //   });
+      //   return present;
+      // })
+      this.filters.every((filterElement) => {
+        this.jobs = this.jobs.filter((job) => {
+          if (job.tools?.includes(filterElement) || job.role?.includes(filterElement) || job.level?.includes(filterElement) || job.languages?.includes(filterElement)) {
+            return true;
+          }
+          return false;
+        })
+      })
+    }
+  }
+
+  checkFilter(value: string) {
+    if (!this.filters.includes(value)) {
+      this.filters.push(value);
+      this.applyFilter("add", value);
+    }
+  }
+  fetchData() {
+    this.jobLIst.getJobs().subscribe((job) => this.jobs = job);
+  }
   clearFilter() {
     this.filters = [];
+    this.fetchData();
   }
   removeFilter(event: string) {
     this.filters = this.filters.filter((val) => val !== event);
+    this.jobs = []
+    // this.fetchData();
+    // if (this.filters.length !== 0)
+    //   setTimeout(()=>this.applyFilter("remove"),10000);
+
+    this.jobLIst.getJobs().subscribe((job) => {
+      this.jobs = job;
+      if (this.filters.length !== 0)
+        this.applyFilter("remove")
+    }
+    );
+
+
   }
 
 }
